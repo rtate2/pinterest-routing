387e7c3c81c37d6bfb6574dd5bb4fe741626773d
Randy Tate <randytatejr@gmail.com>
2020-01-04 13:24:01 -0600

    added navbar

--- src/components/shared/MyNavbar/MyNavbar.js
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -1,12 +1,30 @@
 import React from 'react';
-
-import './MyNavbar.scss';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 class MyNavbar extends React.Component {
+  logMeOut = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  }
+
   render() {
+    const { authed } = this.props;
+
     return (
       <div className="MyNavbar">
-        <h1>IMA Navbar</h1>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="/">Pinterest</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
+            <div class="my-2 mt-lg-0">
+              {authed && (<button className="nav-linsk btn btn-danger" onClick={this.logMeOut}>Logout</button>)}
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
