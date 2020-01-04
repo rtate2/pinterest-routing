ccdcb151ba6b4e26790a51378a756e7845b9d1db
Randy Tate <randytatejr@gmail.com>
2020-01-04 13:11:03 -0600

    navbar wired up

---
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './MyNavbar.scss';
+
+class MyNavbar extends React.Component {
+  render() {
+    return (
+      <div className="MyNavbar">
+        <h1>IMA Navbar</h1>
+      </div>
+    );
+  }
+}
+
+export default MyNavbar;
