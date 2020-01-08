9e0ce373876708c1c91711f4c52d6b29ce29e101
Randy Tate <randytatejr@gmail.com>
2020-01-07 20:37:16 -0600

    added pins

--- src/components/shared/Board/Board.js
+++ src/components/shared/Board/Board.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import boardShape from '../../../helpers/propz/boardShape';
 
@@ -13,11 +14,12 @@ render() {
   const { board } = this.props;
 
   return (
-    <div className="Board">
+    <div className="Board col-4 mb-3">
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">{board.name}</h5>
           <p class="card-text">{board.description}</p>
+          <Link className="btn btn-primary" to={`/board/${board.id}`}>View Board</Link>
         </div>
       </div>
     </div>
---
+++ src/components/shared/Pin/Pin.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import pinShape from '../../../helpers/propz/pinShape';
+
+import './Pin.scss';
+
+class Pin extends React.Component {
+  static propTypes = {
+    pin: pinShape.pinShape,
+  };
+
+  render() {
+    const { pin } = this.props;
+
+    return (
+      <div className="Pin col-4 mb-3">
+        <img src={pin.imageUrl} class="card-img-top" alt="..."></img>
+        <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">{pin.title}</h5>
+        </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default Pin;
