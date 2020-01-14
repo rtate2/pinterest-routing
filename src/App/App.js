b468d5d07a316704ee564253beb7ea914868d67a
Randy Tate <randytatejr@gmail.com>
2020-01-13 20:06:40 -0600

    save pins works

--- src/App/App.js
+++ src/App/App.js
@@ -14,6 +14,7 @@ import Auth from '../components/pages/Auth/Auth';
 import Home from '../components/pages/Home/Home';
 import BoardForm from '../components/pages/BoardForm/BoardForm';
 import SingleBoard from '../components/pages/SingleBoard/SingleBoard';
+import PinForm from '../components/pages/PinForm/PinForm';
 
 const PublicRoute = ({ component: Component, authed, ...rest }) => {
   const routeChecker = (props) => (authed === false ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/', state: { from: props.location } }} />);
@@ -57,6 +58,7 @@ class App extends React.Component {
               <PrivateRoute path="/board/new" exact component={BoardForm} authed={authed} />
               <PublicRoute path="/auth" exact component={Auth} authed={authed} />
               <PrivateRoute path ="/board/:boardId" exact component={SingleBoard} authed={authed} />
+              <PrivateRoute path ="/board/:boardId/pin/new" exact component={PinForm} authed={authed} />
             </Switch>
           </Router>
       </div>
